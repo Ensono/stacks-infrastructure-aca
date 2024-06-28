8d11c6fd81ae30451cb8a98970d3286e29d26b87
Satenderrathee <satender.rathee07@gmail.com>
2024-06-28 11:06:42 +0100

    updated

--- deploy/terraform/azure/provider.tf
+++ deploy/terraform/azure/provider.tf
@@ -6,12 +6,12 @@ terraform {
       version = "~> 3.108.0"
     }
   }
-  # backend "azurerm" {
-  #   resource_group_name  = "ResourceGroupName"  # Name of the resource group that your storage account resides in.
-  #   storage_account_name = "StorageAccountName" # Name of the storage account for your terraform state file.
-  #   container_name       = "tfstate"            # What your container name within the storage account is called.
-  #   key                  = "terraform.tfstate"  # What your state output will be named.
-  # }
+  backend "azurerm" {
+    # resource_group_name  = "ResourceGroupName"  # Name of the resource group that your storage account resides in.
+    # storage_account_name = "StorageAccountName" # Name of the storage account for your terraform state file.
+    # container_name       = "tfstate"            # What your container name within the storage account is called.
+    # key                  = "terraform.tfstate"  # What your state output will be named.
+  }
 }
 
 provider "azurerm" {
