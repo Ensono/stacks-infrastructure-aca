a74ba6e3e53a8c9f78e88f451813356c988ffaa7
Satenderrathee <satender.rathee07@gmail.com>
2024-07-01 12:35:19 +0100

    updated

--- deploy/terraform/azure/outputs.tf
+++ deploy/terraform/azure/outputs.tf
@@ -10,10 +10,10 @@ output "acae_id" {
   value = module.acae.container_app_environment_id
 }
 
-output "acr_name" {
-  value = azurerm_container_registry.acr.name
-}
+# output "acr_name" {
+#   value = azurerm_container_registry.acr.name
+# }
 
-output "acr_id" {
-  value = azurerm_container_registry.acr.id
-}
+# output "acr_id" {
+#   value = azurerm_container_registry.acr.id
+# }
