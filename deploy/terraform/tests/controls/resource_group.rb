4542c555b66c37c666a4662b311ad1356381f0f5
Satenderrathee <satender.rathee07@gmail.com>
2024-07-02 06:43:46 +0100

    updated

---
+++ deploy/terraform/tests/controls/resource_group.rb
@@ -0,0 +1,12 @@
+# Inspec Documentation for further testing development:
+## https://docs.chef.io/inspec/resources/azure_resource_group/
+
+title "Resource Group"
+
+# Ensure that the resource group exists, is in the correct location and
+# has been provisionned successfully
+describe azure_resource_group(name: input("resource_group_name")) do
+  it { should exist }
+  its("location") { should cmp input("region") }
+  its("properties.provisioningState") { should cmp "Succeeded" }
+end
