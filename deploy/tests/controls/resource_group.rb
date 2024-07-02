e948e048ae6414d4d9243dcfde45558edb686fd0
Satenderrathee <satender.rathee07@gmail.com>
2024-07-02 22:19:52 +0100

    updated

--- deploy/tests/controls/resource_group.rb
+++ deploy/tests/controls/resource_group.rb
@@ -5,7 +5,7 @@ title "Resource Group"
 
 # Ensure that the resource group exists, is in the correct location and
 # has been provisionned successfully
-describe azure_resource_group(name: input("resource_group_name")) do
+describe azure_resource_group(name: input("rg_name")) do
   it { should exist }
   its("location") { should cmp input("location") }
   its("properties.provisioningState") { should cmp "Succeeded" }
