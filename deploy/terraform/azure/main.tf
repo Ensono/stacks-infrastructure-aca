53268058814b3003609784825484af6ed84ae9e2
Satenderrathee <satender.rathee07@gmail.com>
2024-07-08 11:51:44 +0100

    updated as per review comments

--- deploy/terraform/azure/main.tf
+++ deploy/terraform/azure/main.tf
@@ -49,11 +49,3 @@ module "acae" {
   create_container_app             = var.create_container_app
   create_rg                        = var.create_rg
 }
-
-# resource "azurerm_container_registry" "acr" {
-#   name                = module.default_label_short.id
-#   location            = azurerm_resource_group.default.location
-#   resource_group_name = azurerm_resource_group.default.name
-#   admin_enabled       = var.acr_admin_enabled
-#   sku                 = var.acr_sku
-# }
--- deploy/terraform/azure/outputs.tf
+++ deploy/terraform/azure/outputs.tf
@@ -9,11 +9,3 @@ output "acae_name" {
 output "acae_id" {
   value = module.acae.container_app_environment_id
 }
-
-# output "acr_name" {
-#   value = azurerm_container_registry.acr.name
-# }
-
-# output "acr_id" {
-#   value = azurerm_container_registry.acr.id
-# }
