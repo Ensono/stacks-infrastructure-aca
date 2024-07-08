22ca4f1385efd7b8431af1335a58653f0d5922d4
Satenderrathee <satender.rathee07@gmail.com>
2024-07-08 22:03:14 +0100

    updated

--- deploy/tests/controls/acae.rb
+++ deploy/tests/controls/acae.rb
@@ -28,6 +28,35 @@
 # end
 
 
+# control "azure-container-app-environment" do
+#   title "Azure Container App Environment"
+#   desc "Ensure that the Azure Container App Environment has been deployed as expected"
+
+#   # Get all resources that match the criteria
+#   resources = azure_generic_resources(resource_group: input("rg_name"), name: input("acae_name"))
+
+#   # Filter for the Container App Environment
+#   acae_resource = resources.where(type: 'Microsoft.App/managedEnvironments').entries.first
+
+#   # Now test the specific resource
+#   describe acae_resource do
+#     it { should_not be_nil }
+    
+#     if acae_resource.nil?
+#       skip "No Azure Container App Environment found matching the criteria"
+#     else
+#       it { should exist }
+#       its('name') { should cmp input("acae_name") }
+#       its('location') { should cmp input("location").downcase }
+#       its('type') { should cmp 'Microsoft.App/managedEnvironments' }
+#       its('provisioningState') { should cmp "Succeeded" }
+
+#       its('createdTime') { should_not be_nil }
+#       its('changedTime') { should_not be_nil }
+#     end
+#   end
+# end
+
 control "azure-container-app-environment" do
   title "Azure Container App Environment"
   desc "Ensure that the Azure Container App Environment has been deployed as expected"
@@ -38,23 +67,14 @@ control "azure-container-app-environment" do
   # Filter for the Container App Environment
   acae_resource = resources.where(type: 'Microsoft.App/managedEnvironments').entries.first
 
-  # Now test the specific resource
-  describe acae_resource do
-    it { should_not be_nil }
-    
-    if acae_resource.nil?
-      skip "No Azure Container App Environment found matching the criteria"
-    else
-      it { should exist }
-      its('name') { should cmp input("acae_name") }
-      its('location') { should cmp input("location").downcase }
-      its('type') { should cmp 'Microsoft.App/managedEnvironments' }
-      its('provisioningState') { should cmp "Succeeded" }
+  describe azure_generic_resource(resource_id: acae_resource.id) do
+    it { should exist }
+    its('name') { should cmp input("acae_name") }
+    its('location') { should cmp input("location").downcase }
+    its('type') { should cmp 'Microsoft.App/managedEnvironments' }
+    its('properties.provisioningState') { should cmp "Succeeded" }
+    its('properties.appLogsConfiguration.destination') { should cmp 'log-analytics' }
+    its('properties.appLogsConfiguration.logAnalyticsConfiguration.customerId') { should_not be_nil }
 
-      # Additional checks based on available attributes
-      its('createdTime') { should_not be_nil }
-      its('changedTime') { should_not be_nil }
-    end
   end
 end
-
