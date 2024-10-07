5bb7368a3d74815b6d69bad6fdf0d2cd8cace436
Bal Purewal <baldip.purewal@ensono.com>
2024-10-07 10:57:59 +0100

    added key vault, acr and app insights

--- deploy/terraform/azure/outputs.tf
+++ deploy/terraform/azure/outputs.tf
@@ -9,3 +9,67 @@ output "acae_name" {
 output "acae_id" {
   value = module.acae.container_app_environment_id
 }
+
+#########################################
+# ACR
+#########################################
+
+output "acr_resource_group_name" {
+  description = "Created ACR resource group Name"
+  value       = var.create_acr ? azurerm_container_registry.registry.0.resource_group_name : var.acr_resource_group
+  depends_on  = [azurerm_resource_group.default]
+}
+
+output "acr_registry_name" {
+  description = "Created ACR name"
+  value       = var.create_acr ? azurerm_container_registry.registry.0.name : var.acr_registry_name
+  depends_on  = [azurerm_resource_group.default]
+}
+
+
+#########################################
+# Key Vault
+#########################################
+output "key_vault_name" {
+  value = var.create_key_vault ? azurerm_key_vault.default.0.name : ""
+}
+
+#########################################
+# Application Insights
+#########################################
+
+output "app_insights_resource_group_name" {
+  value = var.create_app_insights ? azurerm_application_insights.default[0].resource_group_name : ""
+}
+output "app_insights_name" {
+  value = var.create_app_insights ? azurerm_application_insights.default[0].name : ""
+}
+
+output "app_insights_id" {
+  value = var.create_app_insights ? azurerm_application_insights.default[0].id : ""
+}
+
+output "app_insights_instrumentation_key" {
+  value = var.create_app_insights ? azurerm_application_insights.default[0].instrumentation_key  : ""
+  sensitive = true
+}
+
+#########################################
+# Log Analytics
+#########################################
+
+output "log_analytics_resource_group_name" {
+  value = azurerm_log_analytics_workspace.la.resource_group_name
+}
+output "log_analytics_name" {
+  value = azurerm_log_analytics_workspace.la.name
+}
+
+output "log_analytics_id" {
+  value = azurerm_log_analytics_workspace.la.id
+}
+
+output "log_analytics_key" {
+  value = azurerm_log_analytics_workspace.la.primary_shared_key
+  sensitive = true
+}
