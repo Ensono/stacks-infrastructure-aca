4945981ea6b34d8b42ff96ad8d2a15a1d6657167
Bal Purewal <baldip.purewal@ensono.com>
2024-10-09 10:38:05 +0100

    updated infra

--- deploy/terraform/azure/outputs.tf
+++ deploy/terraform/azure/outputs.tf
@@ -1,4 +1,4 @@
-output "rg_name" {
+output "resource_group_name" {
   value = azurerm_resource_group.default.name
 }
 
@@ -27,11 +27,25 @@ output "acr_registry_name" {
 }
 
 
+# If the ACR was created by this deployment then return its name otherwise return the ACR name that has been created by a different deployment
+output "acr_registry_name" {
+  description = "Created ACR name"
+  value       = var.create_acr ? azurerm_container_registry.registry.0.name : var.acr_registry_name
+  depends_on  = [azurerm_resource_group.default]
+}
+
+output "acr_resource_group_name" {
+  description = "Created ACR resource group Name"
+  value       = var.create_acr ? azurerm_container_registry.registry.0.resource_group_name : var.acr_resource_group
+  depends_on  = [azurerm_resource_group.default]
+}
+
 #########################################
 # Key Vault
 #########################################
+# If the Key Vault was created by this deployment then return its name otherwise return the Key Vault name that has been created by a different deployment
 output "key_vault_name" {
-  value = var.create_key_vault ? azurerm_key_vault.default.0.name : null
+  value = var.create_key_vault ? azurerm_key_vault.default.0.name : var.key_vault_name
 }
 
 #########################################
@@ -50,7 +64,7 @@ output "app_insights_id" {
   value = var.create_app_insights ? azurerm_application_insights.default[0].id : null
 }
 
-output "app_insights_instrumentation_key" {
+output "instrumentation_key" {
   value = var.create_app_insights ? azurerm_application_insights.default[0].instrumentation_key  : null
   sensitive = true
 }
